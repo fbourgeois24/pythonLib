bbc1b3011034f1b19103286e0ce5c2aa834be1f2
francois <francoisbourgeois24@gmail.com>
2021-08-15 09:11:25 +0200

    Amélioration sqliteLib

--- sqliteLib.py
+++ sqliteLib.py
@@ -1,31 +1,51 @@
-import os
+import os, sys
 import sqlite3
 import time
 
 """ Utilitaires pour gérer une db sqlite """
 class sqliteDatabase:
-	""" Classe pour la gestion de la DB """
+	""" Classe pour la gestion d'une base de données sqlite
+		Paramètres:
+			dbName = Nom de la DB
+			GUI = Type d'interface graphique utilisée
+				- None (Défaut)
+				- tkinter
 
-	def __init__(self, dbName):
+	"""
+
+	def __init__(self, dbName, GUI=None):
+		""" Constructeur """
 		self.db = ""
 		self.cursor = ""
 		self.name = dbName
+		self.GUI = GUI
+		if self.GUI not in (None, "tkinter"):
+			raise TypeError("Paramètre GUI incorrect")
 
-	def open(self):
+	def open(self, returnFormat='list'):
 		""" Méthode pour se connecter à la base de données """
 		if os.path.exists("./" + self.name):
 			# Si la db est inacessible, on réessaye 
 			for i in range(10):	
 				try:
 					self.db = sqlite3.connect(self.name)
+					if returnFormat == 'dict':
+						self.db.row_factory = sqlite3.Row
 					self.cursor = self.db.cursor()
-				except:
-					time.sleep(0.2)
-				else:
-					break
+					return True
+				except sqlite3.OperationalError as e: #sqlite3.OperationalError: database is locked:
+					if e == "database is locked":
+						time.sleep(0.2)
+					else:
+						print(e)
+						return False
+				except Exception as e:
+					print(e)
+					return False
 
 		else:
-			showwarning("Base de données introuvable !", "La base de données est introuvable, le programme va se fermer !!")
+			if self.GUI == 'tkinter':
+				showwarning("Base de données introuvable !", "La base de données est introuvable, le programme va se fermer !!")
 			sys.exit(0)
 
 
@@ -38,18 +58,25 @@ class sqliteDatabase:
 		self.db.close()
 
 
-	def backup(self):
+	def backup(self, filepath=None):
 		""" Méthode pour faire un backup de la base de données """
 		# Ouvrir une fenêtre pour choisir la destination
-		filepath = asksaveasfilename(title="Destination du backup",filetypes=[('db files','.db')], initialfile="Inventaire_jeux Backup " + time.strftime("%y%m%d%H%M"), defaultextension='.db', initialdir=dbBackupFolder)
+		if self.GUI == 'tkinter':
+			filepath = asksaveasfilename(title="Destination du backup",filetypes=[('db files','.db')], initialfile="Inventaire_jeux Backup " + time.strftime("%y%m%d%H%M"), defaultextension='.db', initialdir=dbBackupFolder)
+		elif filepath is None:
+			raise ValueError("Aucun chemin de backup spécifié")
+		
 		# Copier la DB à l'endroit indiqué
-		if filepath != "":
-			try:
-				copyfile(self.name, filepath)
-			except Exception as e:
-				showwarning("Erreur de backup", "Le backup n'a pas réussi, une erreur c'est produite : " + str(e))
+		if filepath != "" and filepath is not None:
+			if self.GUI == 'tkinter':	
+				try:
+					copyfile(self.name, filepath)
+				except Exception as e:
+					showwarning("Erreur de backup", "Le backup n'a pas réussi, une erreur c'est produite : " + str(e))
+				else:
+					showinfo("Backup réussi", "Le backup est réussi, le fichier a été sauvegardé dans : " + str(filepath))
 			else:
-				showinfo("Backup réussi", "Le backup est réussi, le fichier a été sauvegardé dans : " + str(filepath))
+				copyfile(self.name, filepath)
 
 
 	def execute(self,query):
@@ -66,12 +93,12 @@ class sqliteDatabase:
 				else:
 					print(e)
 					return False
-			else:
+			except Exception as e:
 				print(e)
 				return False
 			
 
-	def exec(self,query, fetch = "all"):
+	def exec(self,query, fetch = "all", returnFormat='list'):
 		""" Méthode pour exécuter un requête et qui gère l'ouverture et la fermeture de la db automatiquement """
 		# Si INSERT ou UPDATE -> commit
 		if "SELECT" in query[:10]:
@@ -80,7 +107,7 @@ class sqliteDatabase:
 			commit = True
 
 		# Exécution de la requête
-		self.open()
+		self.open(returnFormat)
 		if self.execute(query):
 			if (not commit) and fetch == "all":
 				result = self.fetchall()
@@ -88,7 +115,16 @@ class sqliteDatabase:
 				result = self.fetchone()
 			self.close(commit)
 			if not commit:
-				return result
+				if type(result) is None:
+					return ["",]
+				else:
+					if returnFormat == 'dict':
+						listResult = []
+						for item in result:
+							listResult.append(dict(item))
+						return listResult
+					else:
+						return result
 
 
 
